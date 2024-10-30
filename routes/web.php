34360be50389be0400633feeaae5ce5407e43a8b
naufalrahmanu <muhammadnaufalrahmanuanindito@mail.ugm.ac.id>
2024-10-30 21:41:02 +0700

    pertemuan_9

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,11 @@ Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
+Route::middleware(['admin'])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.dashboard');
+});
+
+
 Route::get('/dashboard', function () {
     return 'Ini dashboard, hanya untuk pengguna yang sudah login.';
 })->middleware('auth');
